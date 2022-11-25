07d2c47888b126e2e39af2e88d73c4703e478b35
byungmin12 <kbm940526@naver.com>
2022-11-25 10:21:01 +0900

    다시풀어보기

---
+++ leetcode_75days/33-1day/moveZeroes.js
@@ -0,0 +1,1 @@
+/** * @param {number[]} nums * @return {void} Do not return anything, modify nums in-place instead. */var moveZeroes = function(nums) {    var pos = 0;    for (var i = 0; i < nums.length; i++) {        if (nums[i] !== 0) {            nums[pos++] = nums[i];        }    }    for (i = pos; i < nums.length; i++) {        nums[i] = 0;    }};
