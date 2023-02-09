56b05af242081ef2a03f7a8ad6c3042fce98e9bc
byungmin12 <kbm940526@naver.com>
2023-02-09 10:06:53 +0900

    쉽게 푼 풀이

---
+++ leetcode_study/hash/missing_number.js
@@ -0,0 +1,1 @@
+/** * @param {number[]} nums * @return {number} */var missingNumber = function(nums) {    const memo = new Set(nums)    for(let i =0 ; i<nums.length; i++){        if(!memo.has(i))return i    }    return Math.max(...nums)+1};
