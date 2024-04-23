65b25b8cf54a66e3b51ee04da6deaf4579394d79
byungmin12 <kbm940526@naver.com>
2024-04-23 15:35:05 +0900

    algorithm: solve this algorithm in baekjoon

--- 1.baekjoon/1158/0.js
+++ 1.baekjoon/1158/0.js
@@ -1,48 +1,21 @@
 var fs = require("fs");
 const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const splitStr = process.platform === "linux" ? "\n" : "\n";
+const splitStr = process.platform === "linux" ? "\n" : "\r";
 
-var inputs = fs.readFileSync(filepath, "utf8").toString().trim().split(splitStr)[0].split(" ")
-let left = Array.from({length: Number(inputs[0])}).map((_,index)=>index+1)
-const k = inputs[1]
-let pointer = 0;
-const result = []
+var inputs = fs.readFileSync(filepath, "utf8").toString().trim().split(splitStr)
 
-while (left.length ){
-    const cur = left.shift()
-    if(pointer === k-1){
-        result.push(cur)
-        pointer = 0
-    }
-    else {
-        pointer++
-        left.push(cur)
-    }
+const length = inputs[0]
+const nums = [...inputs.slice(1)]
+
+function f(nums) {
+    const stack = []
+
+    nums.forEach((strNum)=>{
+        const num = Number(strNum)
+        if(num === 0)stack.pop()
+        else stack.push(num)
+    })
+    return stack.length ? stack.reduce((acc,cur)=>acc+cur) : 0
 }
 
-console.log(`<${result.join(", ")}>`)
-
-/**
- * 1234567
- * k번째 사람을 제거
- *
- * L : 456712
- * Result : 3
- *
- * L : 7 1 2 4 5
- * Result : 3 6
- *
- * L : 4 5 7 1
- * Result : 3 6 2
- *
- * L : 1 4 5
- * Result : 3 6 2 7
- *
- * L : 4 1
- * Result : 3 6 2 7 5
- *
- * L : 4
- * Result : 3 6 2 7 5 1
- *
- * */
-
+console.log(f(nums))
