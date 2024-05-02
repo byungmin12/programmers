479cacbc4cd1c544ba78fbcfe605e362e7d783ac
byungmin12 <kbm940526@naver.com>
2024-05-02 15:29:29 +0900

    algorithm: solve this algorithm in baekjoon
    success

--- 1.baekjoon/2573/0.js
+++ 1.baekjoon/2573/0.js
@@ -5,61 +5,72 @@ const splitStr = process.platform === "linux" ? "\n" : "\r";
 var inputs = fs.readFileSync(filepath, "utf8").toString().trim().split(splitStr)
 const [row , col] = inputs.shift().split(" ").map(Number)
 const icebergs = inputs.map(input=> input.split(" ").map(Number))
-
-
-const map = {}
-
-for(let i =1; i<row-1; i++){
-    for(let n =1; n<col-1; n++){
-        if(icebergs[i][n] !== 0)map[`${n} ${i}`] = [n,i]
-    }
-}
+let icebergsVisited
 
 const dx = [1,-1,0,0]
 const dy = [0,0,1,-1]
-let visited = Array.from(Array(row), () =>
-    Array.from(Array(col)).fill(false)
-);
-let result = 0
-for(let key in map){
-    const [x,y] = map[key]
-    let isConnected = false
-    for(let i =0; i<dx.length;i++){
-        if(visited[y+dy[i]][x+dx[i]])continue
-        if(icebergs[y+dy[i]][x+dx[i]] === 0 && icebergs[y][x] > 0){
-            icebergs[y][x]--
-            visited[y][x] = true
-            isConnected = true
+function f(row,col,icebergs) {
+    const map = {}
+    let year = 0
+    for(let i =1; i<row-1; i++){
+        for(let n =1; n<col-1; n++){
+            if(icebergs[i][n] !== 0)map[`${n} ${i}`] = [n,i]
+        }
+    }
+    let visited
+
+    while(Object.keys(map).length > 0){
+        year++
+        visited = Array.from(Array(row), () =>
+            Array.from(Array(col)).fill(false)
+        );
+
+        for(let key in map){
+            const [x,y] = map[key]
+            for(let i =0; i<dx.length;i++){
+                if(visited[y+dy[i]][x+dx[i]]){
+                    continue
+                }
+                if(icebergs[y+dy[i]][x+dx[i]] === 0 && icebergs[y][x] > 0){
+                    icebergs[y][x]--
+                    visited[y][x] = true
+                }
+            }
+
+            if(icebergs[y][x] === 0 ){
+                delete map[key]
+            }
+        }
+
+        let result = 0
+
+        icebergsVisited =   Array.from(Array(row), () => new Array(col).fill(false))
+        for(let key in map){
+            const [x,y] = map[key]
+
+            if(icebergsVisited[y][x] === false ){
+                bfs(x,y)
+                result++
+            }
+        }
+
+        if(result >1 ){
+            return year
         }
     }
 
-    if(icebergs[y][x] === 0 ){
-        delete map[key]
-    }
-    if(isConnected)result++
+    return 0
 }
 
-visited = Array.from(Array(row), () =>
-    Array.from(Array(col)).fill(false)
-);
-result = 0
+console.log(f(row,col,icebergs))
+function bfs(x,y) {
+    icebergsVisited[y][x] = true
 
-for(let key in map){
-    const [x,y] = map[key]
-    let isConnected = false
-    for(let i =0; i<dx.length;i++){
-        if(visited[y+dy[i]][x+dx[i]])continue
-        if(icebergs[y+dy[i]][x+dx[i]] === 0 && icebergs[y][x] > 0){
-            icebergs[y][x]--
-            visited[y][x] = true
-            isConnected = true
-        }
-    }
+    for(let i=0; i<dx.length; i++){
+        const nx = x + dx[i]
+        const ny = y + dy[i]
 
-    if(icebergs[y][x] === 0 ){
-        delete map[key]
+        if(!icebergsVisited[ny] || icebergsVisited[ny][nx] === undefined || icebergsVisited[ny][nx] === true || icebergs[ny][nx] ===0 )continue
+        bfs(nx,ny,icebergsVisited)
     }
-    if(isConnected)result++
 }
-
-console.log(map,result)
